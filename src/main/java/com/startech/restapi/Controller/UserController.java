dde7de04180e5b16d8608766c242bff681afc5ab
Gerbino, Mia <michela.gerbino@cdk.com>
2019-07-23 10:43:44 -0700

    changed to rest controller annotation

--- src/main/java/com/startech/restapi/Controller/UserController.java
+++ src/main/java/com/startech/restapi/Controller/UserController.java
@@ -4,14 +4,14 @@ import com.startech.restapi.Persistence.User;
 import com.startech.restapi.Service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@Controller
+@RestController
 @RequestMapping(path="user/")
 public class UserController {
 
