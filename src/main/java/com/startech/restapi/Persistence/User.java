93128dcb35c37e4e12f1a39beb40a4d2cc2707ff
Gerbino, Mia <michela.gerbino@cdk.com>
2019-07-23 10:56:49 -0700

    user with setter and getters

--- src/main/java/com/startech/restapi/Persistence/User.java
+++ src/main/java/com/startech/restapi/Persistence/User.java
@@ -1,6 +1,6 @@
 package com.startech.restapi.Persistence;
 
-        import javax.persistence.*;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "User")
@@ -23,4 +23,28 @@ public class User {
     }
 
     public User() { }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
